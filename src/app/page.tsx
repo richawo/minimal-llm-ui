6b4e25b78c751d8b8829f6589197d2f29daaae89
Richard Awoyemi <35015261+RichardAwoyemi@users.noreply.github.com>
2023-10-18 13:53:25 +0100

    Added basic context to chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { ChatOllama } from "langchain/chat_models/ollama";
+import { AIMessage, HumanMessage } from "langchain/schema";
 import { useState } from "react";
 
 export default function Home() {
   const [newPrompt, setNewPrompt] = useState("");
+  const [messages, setMessages] = useState<(HumanMessage | AIMessage)[]>([]);
   const [text, setText] = useState("");
 
   const ollama = new ChatOllama({
@@ -12,29 +14,42 @@ export default function Home() {
   });
 
   async function triggerPrompt() {
-      const stream = await ollama.stream(newPrompt);
-      setNewPrompt("");
-      for await (const chunk of stream) {
-        console.log(chunk);
-        setText((prev) => prev + chunk.content);
-      }
+    const size = messages.length;
+    const msg = new HumanMessage(newPrompt);
+    let streamedText = "";
+    messages.push(msg);
+    const msgCache = [...messages];
+    const stream = await ollama.stream(messages);
+    setNewPrompt("");
+    for await (const chunk of stream) {
+      console.log(chunk);
+      streamedText += chunk.content;
+      const aiMsg = new AIMessage(streamedText);
+      setMessages(() => [...msgCache, aiMsg]);
     }
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-16">
-      <div className="flex grow w-full h-full flex-col justify-end items-center">
+      <div className="flex h-full w-full grow flex-col items-center justify-end">
         {/* <button onClick={() => triggerPrompt()}>Click me</button> */}
+        {messages.map((msg, i) => (
+          <p key={i} className="mt-8 flex h-full grow text-sm text-white">
+            {msg.content}
+          </p>
+        ))}
         <p className="mt-8 flex h-full grow text-sm text-white">{text}</p>
         <input
-        // on change, set the new prompt to the value of the input field but if they press enter, trigger the prompt
-          onChange={(e) => {setNewPrompt(e.target.value); (e as any).key === "Enter" && triggerPrompt()}}
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') {
+          // on change, set the new prompt to the value of the input field but if they press enter, trigger the prompt
+          onChange={(e) => {
+            setNewPrompt(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
               // Call your function here
               triggerPrompt();
             }
           }}
-
           id="search"
           className="block w-full appearance-none rounded-md border border-[#191919] bg-[#0a0a0a]/80 px-6 py-4 text-sm font-normal text-white outline-0 focus:outline-0 focus:ring-white/10 md:flex"
           placeholder="Where to travel next year"
