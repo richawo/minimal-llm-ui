f512106968c5ebc6960479cf6e9c09ca4865506c
Richard Awoyemi <35015261+RichardAwoyemi@users.noreply.github.com>
2023-10-19 23:29:30 +0100

    Update msg array data + pulling latest model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,70 @@
 "use client";
-import { cn } from "@/lib/utils";
+import generateRandomString, { cn } from "@/lib/utils";
 import { ChatOllama } from "langchain/chat_models/ollama";
 import { AIMessage, HumanMessage } from "langchain/schema";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [newPrompt, setNewPrompt] = useState("");
-  const [messages, setMessages] = useState<(HumanMessage | AIMessage)[]>([]);
+  const [messages, setMessages] = useState<
+    {
+      type: string;
+      id: any;
+      timestamp: number;
+      content: string;
+      model?: string;
+    }[]
+  >([]);
+  const [availableModels, setAvailableModels] = useState<any[]>([]);
+  const [activeModel, setActiveModel] = useState<string>("");
+  const [ollama, setOllama] = useState<ChatOllama>();
 
-  const ollama = new ChatOllama({
-    baseUrl: "http://localhost:11434",
-    model: "zephyr",
-  });
+  // Get models
+  useEffect(() => {
+    fetch("http://localhost:11434/api/tags")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setAvailableModels(data);
+        console.log(data.models[0]?.name)
+        setActiveModel(data.models[0]?.name);
+        const initOllama = new ChatOllama({
+          baseUrl: "http://localhost:11434",
+          model: data.models[0]?.name,
+        });      
+        setOllama(initOllama)
+      });
+  }, []);
+
 
   async function triggerPrompt() {
-    const size = messages.length;
-    const msg = new HumanMessage(newPrompt);
+    if (!ollama) return;
+    const msg = {
+      type: "human",
+      id: generateRandomString(8),
+      timestamp: Date.now(),
+      content: newPrompt,
+    };
     let streamedText = "";
     messages.push(msg);
     const msgCache = [...messages];
-    const stream = await ollama.stream(messages);
+    const stream = await ollama.stream(
+      messages.map((m) =>
+        m.type == "human"
+          ? new HumanMessage(m.content)
+          : new AIMessage(m.content),
+      ),
+    );
     setNewPrompt("");
     for await (const chunk of stream) {
-      console.log(chunk);
       streamedText += chunk.content;
-      const aiMsg = new AIMessage(streamedText);
-      const updatedMessages = [...msgCache, aiMsg]
+      const aiMsg = {
+        type: "ai",
+        id: generateRandomString(8),
+        timestamp: Date.now(),
+        content: streamedText,
+      };
+      const updatedMessages = [...msgCache, aiMsg];
       setMessages(() => updatedMessages);
     }
   }
@@ -33,32 +72,38 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-16">
       <div className="flex h-full w-full grow flex-col items-center justify-end gap-y-4 whitespace-break-spaces">
-        {messages.map((msg, i) => (
-          <p
-            key={i}
-            className={cn("flex h-fit text-sm text-white rounded-md border border-[#191919] px-2 py-1",
-            {"ml-auto": msg._getType() == "human"},
-            {"mr-auto": msg._getType() == "ai"}
-            )}
-          >
-            {msg.content}
-          </p>
+        {messages.map((msg) => (
+            <p
+              key={"message-" + msg.id}
+              className={cn(
+                "flex h-fit cursor-pointer rounded-md border border-[#191919] px-2 py-1 text-sm text-white",
+                { "ml-auto": msg.type == "human" },
+                { "mr-auto": msg.type == "ai" },
+              )}
+            >
+              {msg.content}
+            </p>
         ))}
         <input
           onChange={(e) => {
             setNewPrompt(e.target.value);
           }}
           onKeyDown={(e) => {
-            if (e.key === "Enter") {
+            if (e.key === "Enter" && !e.metaKey && !e.shiftKey && !e.altKey) {
               triggerPrompt();
+            } else if (
+              e.key === "Enter" &&
+              (e.metaKey || !e.shiftKey || !e.altKey)
+            ) {
+              console.log(e);
             }
           }}
-          id="search"
           className="block w-full appearance-none rounded-md border border-[#191919] bg-[#0a0a0a]/80 px-6 py-4 text-sm font-normal text-white outline-0 focus:outline-0 focus:ring-white/10 md:flex"
           placeholder="Send a message"
           type="text"
           value={newPrompt}
         ></input>
+        <p className="text-xs text-white/50">{activeModel}</p>
       </div>
     </main>
   );
--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export default function generateRandomString(length: number) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+}
