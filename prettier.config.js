1906acc6741dbc69fce7e7f2409e547dbdfdb691
Richard Awoyemi <35015261+RichardAwoyemi@users.noreply.github.com>
2023-10-27 22:53:50 +0100

    linting on prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    plugins: ['prettier-plugin-tailwindcss'],
-    tailwindConfig: './tailwind.config.ts',
-  }
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.ts",
+};
