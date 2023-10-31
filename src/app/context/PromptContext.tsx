8318421cb1d4ebd6e384451cd1d1731665089f08
Richard Awoyemi <35015261+RichardAwoyemi@users.noreply.github.com>
2023-10-31 16:26:24 +0000

    prompt context crud operations

--- src/app/context/PromptContext.tsx
+++ src/app/context/PromptContext.tsx
@@ -7,19 +7,29 @@ export function usePrompts() {
 }
 
 export function PromptsProvider({ children }: { children: React.ReactNode }) {
-  const [promptTemplate, setPromptTemplate] = useState([]);
+  const [promptTemplate, setPromptTemplate] = useState<any[]>([]);
 
   // Load prompts from local storage on component mount
   useEffect(() => {
+    updatePromptTemplate();
+  }, []);
+
+  function updatePromptTemplate() {
     const storedPromptsString = localStorage.getItem("prompts");
 
     if (storedPromptsString) {
       const storedPrompts = JSON.parse(storedPromptsString);
       setPromptTemplate(storedPrompts);
     }
-  }, []);
+  }
 
-  const value = { promptTemplate };
+  function addNewPrompt<T>(prompt: T) {
+    const newPromptTemplate = [...promptTemplate, prompt];
+    setPromptTemplate(newPromptTemplate);
+    localStorage.setItem("prompts", JSON.stringify(newPromptTemplate));
+  }
+
+  const value = { promptTemplate, updatePromptTemplate, addNewPrompt };
 
   return (
     <PromptsContext.Provider value={value}>{children}</PromptsContext.Provider>
