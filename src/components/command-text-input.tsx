0d56746ade5b17bb50d269b2d7cc69c1891cec40
Richard Awoyemi <35015261+RichardAwoyemi@users.noreply.github.com>
2023-11-04 00:04:36 +0000

    Command menu Inputs working

--- src/components/command-text-input.tsx
+++ src/components/command-text-input.tsx
@@ -15,7 +15,7 @@ export default function CommandTextInput({
   placeholder,
   expand = true,
 }: Props) {
-  const { setActivePromptTemplate } = usePrompts();
+  const { activePromptTemplate, setActivePromptTemplate } = usePrompts();
 
   useEffect(() => {
     if (expand && textareaRef && textareaRef.current) {
@@ -34,9 +34,23 @@ export default function CommandTextInput({
       className="flex max-h-[200px] w-full cursor-text resize-none appearance-none rounded-md border border-[#191919] bg-[#0a0a0a]/80 px-6 py-4 text-sm font-normal text-white outline-0 focus:outline-0 focus:ring-white/10 md:flex"
       placeholder={placeholder}
     >
-      <button onClick={() => setActivePromptTemplate(undefined)}>
-        <RightChevron className="h-4 w-4 rotate-180 cursor-pointer fill-white" />
+      <button
+        className="cursor-pointer rounded-md border border-white/10 px-0.5"
+        onClick={() => setActivePromptTemplate(undefined)}
+      >
+        <RightChevron className="h-4 w-4 rotate-180 fill-white hover:fill-white" />
       </button>
+      <div className="flex gap-x-5 px-5 text-xs">
+        {activePromptTemplate.inputs.map((x: string) => (
+          <span
+            className="flex items-center gap-x-2 rounded-md border border-white/10 px-2 py-1"
+            key={x}
+          >
+            <div className="font-bold text-white/50">{x}:</div>
+            <input className="content min-w-[50px] max-w-full bg-transparent outline-none outline-0"></input>
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
