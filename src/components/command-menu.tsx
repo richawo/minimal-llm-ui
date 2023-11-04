83cf3eee2d7b97b9d84aaa913e768fe4a2a91123
Richard Awoyemi <35015261+RichardAwoyemi@users.noreply.github.com>
2023-11-04 01:55:15 +0000

    Fix command menu gap

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -13,21 +13,24 @@ export default function CommandMenu({ showMenu }: Props) {
   return (
     <>
       {showMenu && (
-          <motion.div className="flex flex-col rounded-md border border-[#191919] bg-[#0a0a0a]/80 p-2 text-sm font-normal text-white">
-            <div className="text-xs font-bold text-white/50 px-5 py-1">Command Menu</div>
-            {promptTemplate.map(
-              (t: { name: string; content: string; inputs: string[] }) => (
-                <motion.div
-                  onClick={() => setActivePromptTemplate(t)}  
-                  initial={{ opacity: 0, height: 0 }}
-                  animate={{ opacity: 1, height: "auto" }}
-                  exit={{ opacity: 0, height: 0 }}
-                  layout
-                  layoutId={t.name}
-                  key={t.name}
-                  className="cursor-pointer px-5 py-1 hover:bg-white/[0.03]"
-                >
-                  {t.name} -{" "}
+        <motion.div className="flex flex-col rounded-md border border-[#191919] bg-[#0a0a0a]/80 p-2 text-sm font-normal text-white">
+          <div className="px-5 py-1 text-xs font-bold text-white/50">
+            Command Menu
+          </div>
+          {promptTemplate.map(
+            (t: { name: string; content: string; inputs: string[] }) => (
+              <motion.div
+                onClick={() => setActivePromptTemplate(t)}
+                initial={{ opacity: 0, height: 0 }}
+                animate={{ opacity: 1, height: "auto" }}
+                exit={{ opacity: 0, height: 0 }}
+                layout
+                layoutId={t.name}
+                key={t.name}
+                className="cursor-pointer px-5 py-1 hover:bg-white/[0.03]"
+              >
+                {t.name} -{" "}
+                <div className="inline-flex gap-x-1">
                   {t.inputs?.map((i: string) => (
                     <span
                       className="rounded-md bg-white/10 px-1 py-0.5 text-xs"
@@ -35,16 +38,17 @@ export default function CommandMenu({ showMenu }: Props) {
                     >
                       {i.slice(5)}
                     </span>
-                  ))}{" "}
-                  -{" "}
-                  {t.content.length < 100
-                    ? t.content
-                    : t.content.slice(0, 45) + "..." + t.content.slice(-45)}
-                </motion.div>
-              ),
-            )}
-          </motion.div>
-        )}
+                  ))}
+                </div>{" "}
+                -{" "}
+                {t.content.length < 100
+                  ? t.content
+                  : t.content.slice(0, 45) + "..." + t.content.slice(-45)}
+              </motion.div>
+            ),
+          )}
+        </motion.div>
+      )}
     </>
   );
 }
